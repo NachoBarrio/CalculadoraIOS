fc714acd2e6658b70367ccd63b4f0d72198039ec
naxetete89 <naxetete@hotmail.com>
2018-06-01 21:22:35 +0200

    Initial Commit

---
+++ CalculadoraIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculadoraIOS
+//
+//  Created by Ignacio Barrio Santos on 1/6/18.
+//  Copyright © 2018 Ignacio Barrio Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
